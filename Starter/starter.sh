7cb57f1d31a9d4025f21748e45981a2bd909181a
Haihao Yan <hyan@apdgroup.com>
2017-09-04 13:30:28 +1000

    add all

---
+++ Starter/starter.sh
@@ -0,0 +1,6 @@
+export AWS_ACCESS_KEY_ID=AKIAJUH7KRJXRXIFBFYA
+export AWS_SECRET_ACCESS_KEY=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
+
+pyspark --master spark://0.0.0.0:7077 --packages com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.2
+  q1.py
+  
--- assignment-q6.py
+++ assignment-q6.py
@@ -2,12 +2,12 @@ from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 
 df = sqlContext.read.json('s3a://oxclo-assignment/dev5000.json')
-total = df.rdd.map(lambda row: 1).reduce(lambda a,b: a+b)
 
 has_meta = df.rdd.filter(lambda row: row.metadata is not None and row.metadata.os is not None)
+total_os = has_meta.map(lambda row: 1).reduce(lambda a,b: a+b)
 windows = has_meta.filter(lambda row: row.metadata.os is not None and row.metadata.os.lower() == "windows").map(lambda row: 1).reduce(lambda a,b: a+b)
 ubuntu = has_meta.filter(lambda row: row.metadata.os is not None and row.metadata.os.lower() == "ubuntu").map(lambda row: 1).reduce(lambda a,b: a+b)
 
-print windows
-print ubuntu
+print float (windows)/total_os
+print float (ubuntu)/total_os
 
