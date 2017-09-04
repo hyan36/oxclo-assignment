fe23b0f91966ac573ffce061f51cb0b248ac4c80
Haihao Yan <hyan@apdgroup.com>
2017-09-04 13:30:28 +1000

    add all

--- assignment-q1.py
+++ assignment-q1.py
@@ -1,6 +1,8 @@
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
-df = sqlContext.read.json('s3a://oxclo-assignment/dev.json')
+
+df = sqlContext.read.json('s3a://oxclo-assignment/ip-20170715-sampled.json')
 count = df.rdd.map(lambda row: 1).reduce(lambda a,b: a+b)
 print count
 
