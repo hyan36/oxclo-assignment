36ca182d1466a6ee9c1ae7d46cb0c728a033848f
Haihao Yan <hyan36@gmail.com>
2017-09-05 10:16:06 +1000

    Update starter.sh

--- Starter/starter.sh
+++ Starter/starter.sh
@@ -1,5 +1,5 @@
-export AWS_ACCESS_KEY_ID=AKIAJUH7KRJXRXIFBFYA
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=<Key>
+export AWS_SECRET_ACCESS_KEY=<Secret>
 
 pyspark --master spark://0.0.0.0:7077 --packages com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.2
   q1.py
