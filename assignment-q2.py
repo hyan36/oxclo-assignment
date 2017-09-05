8cbe2613083b5367aa7886b92c43b532ff14ec13
Haihao Yan <hyan36@gmail.com>
2017-09-05 15:21:39 +1000

    Update assignment-q2.py

--- assignment-q2.py
+++ assignment-q2.py
@@ -2,7 +2,10 @@ from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 df = sqlContext.read.json('s3a://oxclo-assignment/dev.json')
 
-countries = df.rdd.filter(lambda row: row.location is not None).map(lambda row: (row.location.country_code, 1)).reduceByKey(lambda a,b: a+b).sortBy(lambda x: x[1], False)
+countries = df.rdd.filter(lambda row: row.location is not None)\
+.map(lambda row: (row.location.country_code, 1))\
+.reduceByKey(lambda a,b: a+b)\
+.sortBy(lambda x: x[1], False)
 
 for k,v in countries.collect()[:10]:
 	print k,v
